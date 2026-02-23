00b81947e26263d2b10772449e138dd5effe3bba
Ashley Cherry <ashleycherry16@gmail.com>
2026-02-22 20:33:43 -0600

    A web application that utilizes TheDogAPI to fetch and display dog images and breed data.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// Test Fetch for TheDogAPI
+const dogUrl = 'https://api.thedogapi.com/v1/images/search?has_breeds=true';
 
+fetch(dogUrl)
+    .then(response => response.json())
+    .then(data => {
+       
+        console.log("Dog API Response:", data);
+        
+        // 2 required data points
+        const breed = data[0].breeds[0].name;
+        const imageUrl = data[0].url;
+        console.log(`Data Point 1 (Breed): ${breed}`);
+        console.log(`Data Point 2 (Image): ${imageUrl}`);
+    })
+    .catch(error => console.error('Error fetching data:', error));
